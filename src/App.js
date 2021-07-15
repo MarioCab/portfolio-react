b6d381adf498d4592748f10e6f6e1dff241c6bd6
Mario Cabrera <yankeescab@gmail.com>
2021-07-15 18:11:59 -0400

    adding to carosuel

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
         <Navbar />
       </div>
 
-      <div className="row justify-content-evenly">
-        <div className="col-3">
+      <div className="row justify-content-evenly" id="abMe">
+        <div className="col-5">
           <img
             id="profilePic"
             className="img-responsive"
@@ -20,14 +20,53 @@ function App() {
             alt="profilePic"
           ></img>
         </div>
-        <div className="col-9">About me text here</div>
-      </div>
-
-      <div class="row">
-        <div id="carousel" className="col">
-          <Carousel />
+        <div className="col-7">
+          <h2>About Me</h2>
+          <p>
+            My name is Mario Cabrera, and welcome to my portfolio page. Use the
+            NavBar to automatically scroll to where you want to be directed to,
+            or click the "Resume" link to open my resume in a seperate tab. I
+            keep this page consistantly updated with my latest projects/contact
+            information, so feel free to check them out or reach out at any
+            time.
+          </p>
+          <p>
+            <p>
+              In summer 2020 I graduated from Georgia College & State University
+              with a Bachelors Degree of Science in Psychology as well as a
+              minor in Criminal Justice. I currently live in Marietta, Georgia,
+              working on my programing skills from home while looking for work.
+            </p>
+            <p>
+              I was fortunate enough to have game consoles, computers, and cell
+              phones go through revolutionary changes while still in my youth,
+              exposing me to all sorts of amazing technology at a young age. As
+              I grew up, I learned how to use all of this amazing technology.
+              After learning how to use it, I became interested in learning how
+              it functions. This led me to build my own PC in middle school,
+              teaching myself how the hardware interacted. I used this kowledge
+              to make a bit of money by fixing hardware issues with my neighbors
+              and friends PC's. Although my degree is in Psychology, I am now
+              persuing a career in programming. I have self-taught throughout
+              the years, however more recently I have taken a bootcamp from
+              Georgia Tech, which has skyrocketed my knowledge. The idea of
+              becoming a developer and gaining more knowledge has been one of
+              the most exciting prospects in my life, once which I am commited
+              to persue. I hope to one day use my Psychology degree paired with
+              programming knowledge and experience to begin work in AI
+              development.
+            </p>
+          </p>
         </div>
       </div>
+      <container>
+        <div class="row" id="projDiv">
+          <h1 className="row justify-content-center">Projects:</h1>
+          <div id="carousel" className="col">
+            <Carousel />
+          </div>
+        </div>
+      </container>
     </>
   );
 }
--- src/components/Carosoul.js
+++ src/components/Carosoul.js
@@ -1,18 +1,13 @@
 import React from "react";
 import mind from "../imgs/minddump.png";
+import meme from "../imgs/mementopic.png";
+import qol from "../imgs/qolpic.png";
 
 function Carousel() {
   return (
     <div className="carousel slide" data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-
-        <div className="carousel-item">
           <img
             src={mind}
             id="minddump"
@@ -26,7 +21,11 @@ function Carousel() {
         </div>
 
         <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..."></img>
+          <img
+            src={meme}
+            className="d-block w-100"
+            alt="screenshot of memento application"
+          ></img>
           <div className="carousel-caption d-none d-md-block">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
@@ -34,7 +33,11 @@ function Carousel() {
         </div>
 
         <div class="carousel-item">
-          <img src="..." className="d-block w-100" alt="..."></img>
+          <img
+            src={qol}
+            className="d-block w-100"
+            alt="image of qol project"
+          ></img>
           <div className="carousel-caption d-none d-md-block">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
